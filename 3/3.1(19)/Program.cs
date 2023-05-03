fbad5920eb1574a0e1fdcc36253297cc603a58e9
ker-laeda <admin@dwhome.ru>
2023-05-03 16:35:47 +0300

    Homework for the third seminar (remaining tasks)

---
+++ 3/3.1(19)/Program.cs
@@ -0,0 +1,27 @@
+﻿bool IsPalindrome(string chkStr)
+{
+    int front_index = 0;
+    int back_index = chkStr.Length - 1;
+
+    while(front_index < back_index)
+    {
+        if (Convert.ToInt32(chkStr[front_index]) != 
+            Convert.ToInt32(chkStr[back_index]))
+        {
+            return false;
+        }
+   
+        front_index++;
+        back_index--;
+    }
+  
+    return true;
+}
+
+Console.Write("Enter a number: ");
+string chkStr = new string(Console.ReadLine());
+ 
+if (IsPalindrome(chkStr))
+    Console.WriteLine("Current number is palindrome");
+else
+    Console.WriteLine("Current number is not a palindrome");
---
+++ 3/3.3(23)/Program.cs
@@ -0,0 +1,19 @@
+﻿void AddCubicDegree(ref string str, int num)
+{
+    const int degree = 3;
+
+    if (str.Length > 0)
+        str += ", ";
+
+    str += Convert.ToString(Math.Pow(num, degree));
+}
+
+Console.Write("Enter a number: ");
+int inNum = Convert.ToInt32(Console.ReadLine());
+
+string outStr = String.Empty;
+
+for (int i = 0; i < inNum; i++)
+    AddCubicDegree(ref outStr, i + 1);
+
+Console.WriteLine($"Table of cubes for numbers [1-{inNum}]: {outStr}");
